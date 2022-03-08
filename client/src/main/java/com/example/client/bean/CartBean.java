662639ae64d2077a9ccb964f99167bdbf59e9f37
BordyV <56049773+BordyV@users.noreply.github.com>
2022-03-08 11:26:49 +0100

    fuck jb

--- client/src/main/java/com/example/client/bean/CartBean.java
+++ client/src/main/java/com/example/client/bean/CartBean.java
@@ -5,4 +5,25 @@ import java.util.List;
 public class CartBean {
     private Long id;
     private List<CartItemBean> products;
+
+    public CartBean(Long id, List<com.example.client.bean.CartItemBean> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<com.example.client.bean.CartItemBean> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<com.example.client.bean.CartItemBean> products) {
+        this.products = products;
+    }
 }
